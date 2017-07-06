69ab5e03dfb9fb8ed219026f1d8fe2ae5fe60cd0
edward.seufert <edward.seufert@TPA1WS01190.corp.autoclubfl.com>
2017-07-06 12:37:46 -0400

    test

---
+++ butter-security/src/main/java/org/mangobutter/configuration/MultiHttpSecurityConfig.java
@@ -0,0 +1,61 @@
+package org.mangobutter.configuration;
+
+import org.mangobutter.controller.RestAuthenticationEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+public class MultiHttpSecurityConfig {
+	
+	@Configuration
+	@Order(1)                                                        
+	public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+		
+		@Autowired
+		private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+		
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+				.antMatcher("/api/**") 
+				.exceptionHandling()
+					.authenticationEntryPoint(restAuthenticationEntryPoint)
+				.and()
+				.authorizeRequests()
+					.antMatchers("/api/login/**").permitAll()
+					.anyRequest().authenticated();
+		}
+	}
+
+	@Configuration                                                   
+	public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+		@Autowired
+	    private AccessDeniedHandler accessDeniedHandler;
+		
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+            .authorizeRequests()
+				.antMatchers("/", "/login/**", "/public/**").permitAll()
+				.anyRequest().authenticated()
+            .and()
+            .formLogin()
+				.loginPage("/login/login.html")
+				.permitAll()
+				.and()
+            .logout()
+            	.logoutUrl("/logout.html")
+            	.invalidateHttpSession(true)
+				.and()
+			.exceptionHandling()
+				.accessDeniedHandler(accessDeniedHandler);
+		}
+	}
+}
