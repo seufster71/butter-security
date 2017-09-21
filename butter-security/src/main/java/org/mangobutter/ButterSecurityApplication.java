08c0e4c4a7ab844779cbf30fd7caf8acd39cc1a2
edward.seufert <edward.seufert@TPA1WS01190.corp.autoclubfl.com>
2017-09-21 10:29:44 -0400

    test

--- butter-security/src/main/java/org/mangobutter/ButterSecurityApplication.java
+++ butter-security/src/main/java/org/mangobutter/ButterSecurityApplication.java
@@ -2,8 +2,10 @@ package org.mangobutter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"org.mangobutter","org.testo"})
 public class ButterSecurityApplication {
 
 	public static void main(String[] args) {
